13f06b966399ddb5b38af00a7ad3059651ffd77b
supermomonga <hasegawa@supermomonga.com>
2013-07-09 07:24:17 +0900

    scale fix

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -31,25 +31,11 @@ bm = Benchmark.new
 sorter = InsertionSort.new
 
 production = [10, 100, 1000, 5000, 10000, 100000, 200000]
-test       = [10, 100, 1000, 10000, 20000, 30000 ]
+test       = [10, 100, 1000, 10000, 20000, 30000, 40000, 50000, 100000 ]
 
 try = 3
 
 bm.exec sorter, test, [:random, :identical, :asc, :desc], try
-
-# production.tap do |scales|
-#   [:random, :identical, :asc, :desc].each do |datatype|
-#     puts "#{ datatype } array"
-#     scales.each do |scale|
-#       score = try.times.inject(0) do |total|
-#         bm.setup ds.send datatype, scale
-#         score = bm.bench sorter
-#         total += score
-#       end / try
-#       puts "#{ score / 1000 / 1000 } milli seconds for sorting #{ scale } elements' array."
-#     end
-#   end
-# end
 
 
 
